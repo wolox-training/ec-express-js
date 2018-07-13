52df54481c70792075d09018234c38b637dd5130
carzoglioemanuel <emanuel.carzoglio@wolox.com.ar>
2018-07-13 11:41:02 -0300

    -

--- app/routes.js
+++ app/routes.js
@@ -1,1 +1,7 @@
-exports.init = app => {};
+const user = require('./controllers/users'),
+  validation = require('./validations/validations'),
+  { check, validationResult } = require('express-validator/check');
+
+exports.init = app => {
+  app.post('/users', validation.userValidation, user.create);
+};
